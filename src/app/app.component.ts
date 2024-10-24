4f9e6d5eee23bb64f15b7b302fbb9898e13dcfe2
Aline Rabello <aline.rabello.jose@gmail.com>
2024-10-24 15:36:18 -0300

    feat(app): add formcontrolname

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { PoFieldModule, PoInfoModule } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PoInfoModule, PoFieldModule, ReactiveFormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'comapardorCDB-LCI';
+export class AppComponent implements OnInit {
+  public title = 'Comparador CDB-LCI';
+  public form = new FormGroup({
+    cdbValue: new FormControl(0),
+    lciValue: new FormControl(0),
+    daysValue: new FormControl(0),
+    cdbResult: new FormControl(0),
+    lciResult: new FormControl(0),
+    irValue: new FormControl(0),
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+
+  }
+
 }
